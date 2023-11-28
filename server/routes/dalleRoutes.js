b786f0f19a6c1a57d766d4de5f12b8d0ad3be3f1
Pokepoison <jordyaw7@gmail.com>
2023-11-27 22:16:06 -0500

    debugs

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -31,11 +31,11 @@ router.route('/').post(async (req, res) => {
       size: '1024x1024',
       response_format: 'b64_json',
     });
-    const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse?.data?.data[0]?.b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    res.status(500).json(error?.response?.data?.error.message || {message: "Something went wrong"});
   }
 });
 
