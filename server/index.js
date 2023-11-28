241d1ce6904c23eafbf9ccaaa8f6882f494d39c1
Pokepoison <jordyaw7@gmail.com>
2023-11-28 00:55:47 -0500

    update

--- server/index.js
+++ server/index.js
@@ -15,6 +15,16 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+
 app.get('/', async (req, res) => {
   res.status(200).json({
     message: 'Hello from DALL.E!',
