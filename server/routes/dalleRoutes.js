9f3d6ea369dd0e99be52d53f46548da9f08eead6
Pokepoison <jordyaw7@gmail.com>
2023-11-27 23:07:49 -0500

    update

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -31,7 +31,9 @@ router.route('/').post(async (req, res) => {
       size: '1024x1024',
       response_format: 'b64_json',
     });
-    const image = aiResponse?.data?.data[0]?.b64_json;
+    const imageResponse = aiResponse
+    console.log({imageResponse})
+    const image = aiResponse?.data[0]?.b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
