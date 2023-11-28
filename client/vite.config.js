731b652267e4bb6b1f17126a2973c7b74d8fa19f
Pokepoison <jordyaw7@gmail.com>
2023-11-28 01:19:55 -0500

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:8080',
+        secure: false,
+        changeOrigin: true,
+      }
+    }
+  }
+});
+
