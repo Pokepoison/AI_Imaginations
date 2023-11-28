e5d6f0b73ea202ed0f5596c6620bacb4b02b97f6
Pokepoison <jordyaw7@gmail.com>
2023-11-28 00:05:09 -0500

    fix server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
-
+const PORT = process.env.PORT || 8080
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+    app.listen(PORT, () => console.log('Server started on port http://localhost:8080'));
   } catch (error) {
     console.log(error);
   }
