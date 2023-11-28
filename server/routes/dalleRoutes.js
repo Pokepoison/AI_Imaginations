5e2b9e59031cbfe23a09856447b170f16c4e1861
Pokepoison <jordyaw7@gmail.com>
2023-11-27 21:25:26 -0500

    update

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
-const router = express.Router();
-
-const configuration = new Configuration({
+const openAI = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+// console.log({openAI})
+const router = express.Router();
 
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
@@ -20,13 +25,12 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openAI.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
-
     const image = aiResponse.data.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
